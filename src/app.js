56fa46ace1e681e77a189b121d13df05ff51a279
Jorge-coder02 <jorgitenis@hotmail.es>
2025-05-22 09:57:37 +0200

    fixes

--- src/app.js
+++ src/app.js
@@ -7,28 +7,28 @@ const { MONGODB_URI } = process.env;
 
 const app = express();
 
-// Middlewares
+// Middleware CORS
 const allowedOrigins = [
   "https://growbit.netlify.app",
   "http://localhost:5174",
   "http://localhost:3000",
 ];
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      // Permite solicitudes sin origen (como apps móviles o curl)
-      if (!origin) return callback(null, true);
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       // Permite solicitudes sin origen (como apps móviles o curl)
+//       if (!origin) return callback(null, true);
 
-      if (allowedOrigins.indexOf(origin) === -1) {
-        const msg = `El origen ${origin} no tiene permiso de acceso.`;
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
-    credentials: true,
-  })
-);
+//       if (allowedOrigins.indexOf(origin) === -1) {
+//         const msg = `El origen ${origin} no tiene permiso de acceso.`;
+//         return callback(new Error(msg), false);
+//       }
+//       return callback(null, true);
+//     },
+//     credentials: true,
+//   })
+// );
 app.use(express.json());
 
 // Rutas
@@ -36,7 +36,7 @@ app.use("/api", userRoutes); // Prefijo para todas las rutas del backend
 
 // Conexión a MongoDB
 mongoose
-  .connect(process.env.MONGODB_URI)
+  .connect(MONGODB_URI)
   .then(() => console.log("✅ Conectado a MongoDB"))
   .catch((err) => console.error("Error de conexión:", err));
 
